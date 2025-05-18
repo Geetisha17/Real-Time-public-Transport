8b3550216c34bc5a7bc0b2cf396442b22201f0ca
Geetisha Tandon <geetishatandon@gmail.com>
2025-05-18 16:34:00 +0000

    frontend updated

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,22 +1,16 @@
+import 'react-native-reanimated'; 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MapScreen from './mapScreen';
+import MapScreen from './screens/MapScreen';
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <MapScreen/>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <MapScreen />
+        <StatusBar style="auto" />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
